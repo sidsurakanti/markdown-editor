fb34d52ea67ac89ca3a92156b957a9615c88c555
Sid <izzyy.wq@gmail.com>
2024-03-20 16:41:05 -0400

    styles: added toolbar icons

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -2,17 +2,30 @@
 
 import { useSlate } from "slate-react";
 import { Button } from "@/components/ui/button";
+import {
+	BoldIcon,
+	CodeIcon,
+	ItalicIcon,
+	UnderlineIcon,
+} from "@/components/ui/icons";
 
 export function Toolbar() {
 	const editor = useSlate();
 
-
 	return (
-		<section className="fixed mx-auto bottom-10 bg-rose-500 inset-x-0 w-fit px-4 py-2 rounded-full flex justify-around gap-4">
-			<Button>B</Button>
-			<Button>I</Button>
-			<Button>U</Button>
-			<Button>code</Button>
+		<section className="fixed mx-auto bottom-10 bg-rose-200 inset-x-0 w-fit p-1 rounded-full flex justify-around gap-1">
+			<Button variant="ghost" size="icon" className="rounded-full">
+				<BoldIcon />
+			</Button>
+			<Button variant="ghost" size="icon" className="rounded-full">
+				<ItalicIcon />
+			</Button>
+			<Button variant="ghost" size="icon" className="rounded-full">
+				<UnderlineIcon />
+			</Button>
+			<Button variant="ghost" size="icon" className="rounded-full">
+				<CodeIcon />
+			</Button>
 		</section>
 	);
 }
--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -57,3 +57,20 @@ export function ItalicIcon(
 		</svg>
 	);
 }
+
+export function CodeIcon(props: SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="1em"
+			height="1em"
+			viewBox="0 0 24 24"
+			{...props}
+		>
+			<path
+				fill="currentColor"
+				d="M4.825 12.025L8.7 15.9q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275l-4.6-4.6q-.15-.15-.213-.325T2.426 12q0-.2.063-.375T2.7 11.3l4.6-4.6q.3-.3.713-.3t.712.3q.3.3.3.713t-.3.712zm14.35-.05L15.3 8.1q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375q0 .2-.062.375t-.213.325l-4.6 4.6q-.3.3-.7.288t-.7-.313q-.3-.3-.3-.712t.3-.713z"
+			></path>
+		</svg>
+	);
+}
