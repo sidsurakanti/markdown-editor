64940e1d1f5405edc63457f259dbb571031933ea
Sid <izzyy.wq@gmail.com>
2024-03-21 15:47:26 -0400

    refactor: got rid of unused methods

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -25,45 +25,6 @@ export const UpdatedEditor = {
 		}
 	},
 
-	isCodeBlock: function (editor: CustomEditor): boolean {
-		// downlevel iteration needs to be enabled for this is needed
-		// bc of a bug w next.js and typescript
-		const [match]: Generator<NodeEntry<Node>> = Editor.nodes(editor, {
-			match: (n: Node) => Element.isElement(n) && n.type === "code",
-		});
-
-		console.log(match);
-		return !!match;
-	},
-
-	toggleCodeBlock: function (editor: CustomEditor): void {
-		const isActive = this.isCodeBlock(editor);
-
-		Transforms.setNodes(
-			editor,
-			{ type: isActive ? undefined : "code" },
-			{ match: (n: Node) => Element.isElement(n) && Editor.isBlock(editor, n) }
-		);
-	},
-
-	isQuoteBlock: function (editor: CustomEditor): boolean {
-		const [match]: Generator<NodeEntry<Node>> = Editor.nodes(editor, {
-			match: (n: Node) => Element.isElement(n) && n.type === "quote",
-		});
-
-		return !!match;
-	},
-
-	toggleQuoteBlock: function (editor: CustomEditor): void {
-		const isActive = this.isQuoteBlock(editor);
-
-		Transforms.setNodes(
-			editor,
-			{ type: isActive ? undefined : "quote" },
-			{ match: (n: Node) => Element.isElement(n) && Editor.isBlock(editor, n) }
-		);
-	},
-
 	isBlockType: function (
 		editor: CustomEditor,
 		type: CustomElement["type"]
